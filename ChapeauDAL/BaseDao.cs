af9eef588131f0ae70597f767250a74778e31e50
Edgerunner0 <699380@student.inholland.nl>
2024-05-06 11:06:50 +0200

    Add project files.

---
+++ ChapeauDAL/BaseDao.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace ChapeauDAL
+{                                                           //maybe need to change somethings in this file.
+    public abstract class BaseDao
+    {
+        private SqlDataAdapter adapter;
+        private SqlConnection conn;
+
+        public BaseDao()
+        {
+            // DO NOT FORGET TO INSERT YOUR CONNECTION STRING NAMED 'SOMEREN DATABASE' IN YOUR APP.CONFIG!!
+            /*
+                conn = new SqlConnection(ConfigurationManager.ConnectionStrings["SomerenDatabase"].ConnectionString);
+                adapter = new SqlDataAdapter();
+             */
+            conn = new SqlConnection(ConfigurationManager.ConnectionStrings["SomerenDatabase"].ConnectionString);
+            adapter = new SqlDataAdapter();
+        }
+
+        protected SqlConnection OpenConnection()
+        {
+            try
+            {
+                if (conn.State == ConnectionState.Closed || conn.State == ConnectionState.Broken)
+                {
+                    conn.Open();
+                }
+            }
+            catch (Exception e)
+            {
+                //Print.ErrorLog(e);
+                throw;
+            }
+            return conn;
+        }
+
+        private void CloseConnection()
+        {
+            conn.Close();
+        }
+
+        /* For Insert/Update/Delete Queries with transaction */
+        protected void ExecuteEditTranQuery(string query, SqlParameter[] sqlParameters, SqlTransaction sqlTransaction)
+        {
+            SqlCommand command = new SqlCommand(query, conn, sqlTransaction);
+
+            try
+            {
+                command.Parameters.AddRange(sqlParameters);
+                adapter.InsertCommand = command;
+                command.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                //Print.ErrorLog(e);
+                throw;
+            }
+            finally
+            {
+                CloseConnection();
+            }
+        }
+
+        /* For Insert/Update/Delete Queries */
+        protected void ExecuteEditQuery(string query, SqlParameter[] sqlParameters)
+        {
+            SqlCommand command = new SqlCommand();
+
+            try
+            {
+                command.Connection = OpenConnection();
+                command.CommandText = query;
+                command.Parameters.AddRange(sqlParameters);
+                adapter.InsertCommand = command;
+                command.ExecuteNonQuery();
+            }
+            catch (SqlException e)
+            {
+                // Print.ErrorLog(e);
+                throw;
+            }
+            finally
+            {
+                CloseConnection();
+            }
+        }
+
+        /* For Select Queries */
+        public object ExecuteScalarQuery(string query, SqlParameter[] parameters)
+        {
+            using (SqlConnection connection = new SqlConnection(/* Your connection string */))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddRange(parameters);
+                    connection.Open();
+                    return command.ExecuteScalar();
+                }
+            }
+        }
+
+        protected DataTable ExecuteSelectQuery(string query, params SqlParameter[] sqlParameters)
+        {
+            SqlCommand command = new SqlCommand();
+            DataTable dataTable;
+            DataSet dataSet = new DataSet();
+
+            try
+            {
+                command.Connection = OpenConnection();
+                command.CommandText = query;
+                command.Parameters.AddRange(sqlParameters);
+                command.ExecuteNonQuery();
+                adapter.SelectCommand = command;
+                adapter.Fill(dataSet);
+                dataTable = dataSet.Tables[0];
+            }
+            catch (SqlException e)
+            {
+                // Print.ErrorLog(e);
+                throw;
+            }
+            finally
+            {
+                CloseConnection();
+            }
+
+            return dataTable;
+        }
+    }
+}
