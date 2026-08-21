d46f230d5ce460e348637a4cc4a0fa56f9361ff6
“Eric-Tsui.cc” <erictsui.cc@gmail.com>
2026-08-21 15:15:25 +0800

    feat: add timestamp for order

--- src/Chapeau.Core/Entities/Order.cs
+++ src/Chapeau.Core/Entities/Order.cs
@@ -1,17 +1,16 @@
 using Chapeau.Core.Enums;
+
 namespace Chapeau.Core.Entities;
 
 public class Order : BaseEntity
 {
     public int TableId { get; set; }
-    public Table?Table { get; set; }
-    
+    public Table? Table { get; set; }
+    public DateTime CreatedAt { get; set; }
     public int EmployeeId { get; set; }
     public Employee? Employee { get; set; }
-    
+
     public OrderStatus Status { get; set; }
     public bool IsPaid { get; set; }
     public List<OrderItem> Items { get; set; } = new();
-    
-
 }
